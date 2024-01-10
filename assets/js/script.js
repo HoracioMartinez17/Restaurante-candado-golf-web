865bb0b44cb3382babbcc2881020c612000e623f
Horacio Martinez <litstyle17@gmail.com>
2024-01-10 23:28:30 +0100

    feat: Implementación de preloader, navbar, header y slider en la página
    Se añadieron las siguientes funcionalidades:
    - Preloader que muestra la carga de la página al inicio.
    - Navbar con animación al hacer clic en los elementos de navegación.
    - Header que se oculta al hacer scroll hacia abajo y se muestra al hacer scroll hacia arriba.
    - Slider en la sección hero con botones de navegación y función de auto-slide.
    
    Se agregaron funciones auxiliares para mejorar la modularidad del código y se realizaron ajustes en el estilo. Además, se implementó la lógica para el auto-slide en el slider y se manejaron eventos de ratón para detener temporalmente el auto-slide al pasar el ratón sobre los botones del slider.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,108 @@
 
 const preloader = document.querySelector("[data-preaload]");
 
-window.addEventListener("load", function() {
-preloader.classList.add("loaded");
-document.body.classList.add("loaded");
-})
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+// añadir evento listenner en multiples elementos
+
+const addEventOnElements = (elements, eventType, callback) => {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+// navbar
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const togglersNavbar = () => {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", togglersNavbar);
+
+// header
+
+const header = document.querySelector("[data-header]");
+
+let lastScrollPos = 0;
+
+const hideHeader = () => {
+  const isCrollBottom = lastScrollPos < window.scrollY;
+  if (isCrollBottom) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+  lastScrollPos = window.scrollY;
+};
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 50) {
+    header.classList.add("active");
+    hideHeader();
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+// hero slider
+
+const heroSlider = document.querySelector("[data-hero-slider]");
+const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
+const heroSliderPrevBtn = document.querySelector("[data-prev-btn]");
+const heroSliderNextvBtn = document.querySelector("[data-next-btn]");
+
+let currentSlidePos = 0;
+let lastActiveSliderItem = heroSliderItems[0];
+
+const updateSliderPosition = () => {
+  lastActiveSliderItem.classList.remove("active");
+  heroSliderItems[currentSlidePos].classList.add("active");
+  lastActiveSliderItem = heroSliderItems[currentSlidePos];
+};
+
+const slideNext = () => {
+  if (currentSlidePos >= heroSliderItems.length - 1) {
+    currentSlidePos = 0;
+  } else {
+    currentSlidePos++;
+  }
+  updateSliderPosition();
+};
+
+heroSliderNextvBtn.addEventListener("click", slideNext);
+
+const slidePrev = () => {
+  if (currentSlidePos <= 0) {
+    currentSlidePos = heroSliderItems.length - 1;
+  } else {
+    currentSlidePos--;
+  }
+  updateSliderPosition();
+};
+heroSliderPrevBtn.addEventListener("click", slidePrev);
+
+// auto slide
+
+let autoSlideInterval;
+
+const autoSlide = () => {
+  autoSlideInterval = setInterval(() => {
+    slideNext();
+  }, 8000);
+};
+
+addEventOnElements([heroSliderNextvBtn, heroSliderPrevBtn], "mouseover", () => {
+  clearInterval(autoSlideInterval);
+});
+addEventOnElements([heroSliderNextvBtn, heroSliderPrevBtn], "mouseout", autoSlide);
+
+window.addEventListener("load", autoSlide);
